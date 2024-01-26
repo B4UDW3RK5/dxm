962b0b951642079c0f1c785ce52723020548e4d2
cat K. <cat@b4udw3rk5.com>
2024-01-26 12:26:24 -0800

    added converted dose (this is specifically for 30mg freebase pills for now but I might add config later for syrups, hBR, etc)

--- dxm.sh
+++ dxm.sh
@@ -7,14 +7,16 @@ DATA_FILE="/Users/cat/dxm.csv"
 if [ "$#" -eq 1 ]; then
     # ...timestamp it...
     timestamp=$(date +%s)
-    # ...and add it to the end of the csv
-    echo "$timestamp,$1" >> "$DATA_FILE"
+    #calculate the mg
+    dose=$(echo "$1 * 30" | bc)
+    # ...and add it all to the end of the csv
+    echo "$timestamp,$1,$dose" >> "$DATA_FILE"
 # if no integer is provided...
 else
     # ...return the last entry of the .csv...
     last_line=$(tail -n 1 "$DATA_FILE")
-    # ...convert the timestamp and value to variables...
-    IFS=, read -r timestamp value <<< "$last_line"
+    # ...convert the epoch timestamp, quantity and dose to variables...
+    IFS=, read -r timestamp count dose <<< "$last_line"
     # ...and print them
-    echo "$value@$timestamp"
+    echo "$timestamp,$count,$dose"
 fi
